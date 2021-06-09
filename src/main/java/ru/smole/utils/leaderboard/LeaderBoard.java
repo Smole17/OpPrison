d4d0dae9dc632706a68ff203fbb4315e345c2082
xfenilafs <znikin12@bk.ru>
2021-06-09 19:52:26 +0300

    update tops

--- src/main/java/ru/smole/utils/leaderboard/LeaderBoard.java
+++ src/main/java/ru/smole/utils/leaderboard/LeaderBoard.java
@@ -13,24 +13,27 @@ import ru.xfenilafs.core.util.ChatUtil;
 
 public class LeaderBoard {
     private final ProtocolHolographic hologram;
+    private final String topName;
     private final String criteria;
 
-    public LeaderBoard(Location location, OpPrison plugin, String criteria) {
+    public LeaderBoard(String topName, Location location, String criteria) {
+        this.topName = topName;
         this.criteria = criteria;
         hologram = ApiManager.createHolographic(location);
         createLeaderBoard();
-        plugin.getServer().getScheduler().runTaskTimer(plugin, this::update, 0L, OpPrison.getInstance().getConfig().getInt("leaderBoard.update") * 20L);
+        OpPrison.getInstance().getServer().getScheduler().runTaskTimer(OpPrison.getInstance(), this::update, 0L, OpPrison.getInstance().getConfig().getInt("leaderBoard.update") * 20L);
     }
 
     private void createLeaderBoard() {
-        for (int i = 0; i < 10; i++)
+        hologram.setTextLine(0, ChatUtil.color(topName));
+        for (int i = 1; i < 10; i++)
             hologram.setTextLine(i, ChatUtil.text("#%s §fЗагрузка", i));
     }
 
     public void update() {
         ResultSet resultSet = OpPrison.getInstance().getBase().getResult("SELECT * FROM OpPrison ORDER BY " + criteria + " DESC LIMIT 10");
         try {
-            for (int i = 0; resultSet.next(); i++) {
+            for (int i = 1; resultSet.next(); i++) {
                 hologram.setTextLine(i + 1, getLine(i + 1, resultSet.getString("name"), resultSet.getDouble(criteria)));
             }
         } catch (SQLException exception) {
